95f1d1853c2ac9ded62b7593b0813574616a6f9f
Matyáš Vojan <vojanmatyas@gmail.com>
2026-07-22 16:51:47 +0200

    huhu

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -94,7 +94,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", type: "image/svg+xml", href: "/images/logo/ponici-logo-icon.svg" },
+      { rel: "icon", type: "image/svg+xml", href: "/images/logo/favicon-p.svg" },
       { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
       { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
       { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon-180x180.png" },
